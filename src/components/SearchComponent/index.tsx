e4773c6e82bbe8c7744e7a898cea12e2ee11b878
Vladislav Nazarov <68848418+vladNew91@users.noreply.github.com>
2023-01-13 16:33:27 +0100

    fix: adaptive search component

--- src/components/SearchComponent/index.tsx
+++ src/components/SearchComponent/index.tsx
@@ -11,7 +11,7 @@ const Search = styled("div")(({ theme }) => ({
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: "100%",
+  width: "20ch",
   [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
     width: "auto",
